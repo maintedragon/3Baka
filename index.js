7de16b826f781d04aa26c73ccb2777c5e68c177f
naccoromon <maintedragon@gmail.com>
2022-03-05 15:54:53 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var a =  ;
+if(a > 0){
+    for(var i = 1; a > i; i++){
+        var ii = String(i);
+        if(i % 3 === 0 || ii.indexOf("3") >= 0){
+            document.write(i + "アホ<br>");
+        } else {
+            document.write(i + "<br>");
+    }
+} else {
+    document.write("正の数値にしてください。");
+}
